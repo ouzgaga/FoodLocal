eddb716093c7a7fd6f0dd73e3bcfbb47c6edf5d5
Unknown <ben17_1@hotmail.com>
2018-10-16 12:50:50 +0200

    Partie connection à la DB commentée

--- API/src/app.js
+++ API/src/app.js
@@ -7,13 +7,14 @@ const config = require('./config/config');
 
 mongoose.Promise = require('bluebird');
 
+/*
 mongoose.connect(config.db, { useMongoClient: true });
 const db = mongoose.connection;
 
 db.on('error', () => {
   throw new APIError(`unable to connect to database at ${config.db}`, httpStatus.INTERNAL_SERVER_ERROR);
 });
-
+*/
 const app = express();
 
 module.exports = require('./config/express')(app, config);
