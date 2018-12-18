c86fd027b92762c291191e2688ff28edf8e7c56c
James <james.smith@heig-vd.ch>
2018-12-18 19:45:10 +0100

    Debug script deployment db

--- Deployment/DB/script/DeployDB.sh
+++ Deployment/DB/script/DeployDB.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env bash
 
 # Using docker to connect to our kubernetes on google cloud
-docker-compose up --build gce
-kubectl exec mongo-0 -- rm -r dumpDBProd.agz
-kubectl exec mongo-0 -- mongo api_foodlocal_production --eval "db.dropDatabase()"
-kubectl cp ./dataBase/dumpDBProd.agz mongo-0:dumpDBProd.agz
-kubectl exec mongo-0 -- mongorestore --verbose --gzip --archive=/dumpDBProd.agz
+docker-compose up --build
