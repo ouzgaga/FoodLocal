f90c568b589e0288df53099adcaf974650912181
James <james.smith@heig-vd.ch>
2019-01-02 16:49:54 +0100

    updating script db (to continue)

--- Deployment/DB/script/DeployDB.sh
+++ Deployment/DB/script/DeployDB.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Using docker to connect to our kubernetes on google cloud
-docker build -t gce_foodlocal ./../../Docker/Dockerfile
+docker build -t gce_foodlocal:latest ./../../Docker/
 docker build -t gce_kubectl .
 rm ./../../Docker/tmp/kubectlCmd.sh # remove old script
 cp kubectlCmd.sh ./../../Docker/tmp/kubectlCmd.sh # copy new script
