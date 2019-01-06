ae0273ddba55eec26980d4a63904d0307a509200
James <james.smith@heig-vd.ch>
2019-01-06 15:27:39 +0100

    Update script and gitlab-ci

--- Deployment/DB/script/DeployDB.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Using docker to connect to our kubernetes on google cloud
-docker build -t gce_foodlocal:latest ./../../Docker/
-docker build -t gce_kubectl .
-rm ./../../Docker/tmp/kubectlCmd.sh # remove old script
-cp kubectlCmd.sh ./../../Docker/tmp/kubectlCmd.sh # copy new script
-cd ./../../Docker/ # go into the tmp containing docker
-docker-compose up --build
