ebb476b922549bb8d0e845ec5011f31d166d58e2
James <james.smith@heig-vd.ch>
2019-01-06 15:27:59 +0100

    update script deploy gce

--- Deployment/GCE/kubectlCmd.sh
+++ Deployment/GCE/kubectlCmd.sh
@@ -1,16 +1,18 @@
 #!/usr/bin/env bash
 
-env # show the env variables
+# Deploy DB
+kubectl apply -f ./DB/db-mongo-storage.yml
+kubectl apply -f ./DB/db-clusterRoleBinding.yml
+kubectl apply -f ./DB/db-mongo-service.yml
+kubectl apply -f ./DB/db-mongo-statefulset.yml
 
-# Create the connection with the google cloud account
-gcloud components install kubectl
-gcloud auth activate-service-account --key-file=./gc-key.json # put your gc-key into the same directory
-gcloud container clusters get-credentials $GC_CLUSTER_ID --zone $GC_ZONE --project $GC_PROJECT_ID # will take the information from the .env in the same directory
+# Ingress
+kubectl apply -f ./Ingress/ingress.yml
 
-# Run the kubectl cmd to change the db with the db into the folder dataBase
-kubectl exec mongo-0 -- rm -r dumpDBProd.agz
-kubectl exec mongo-0 -- mongo api_foodlocal_production --eval "db.dropDatabase()"
-kubectl exec mongo-0 -- mongo api_foodlocal_dev --eval "db.dropDatabase()"
-kubectl exec mongo-0 -- mongo api_foodlocal_test --eval "db.dropDatabase()"
-kubectl cp dumpDBProd.agz mongo-0:/dumpDBProd.agz
-kubectl exec mongo-0 -- mongorestore --verbose --gzip --archive=/dumpDBProd.agz
+# Backend (API)
+kubectl apply -f ./API/api-service.yml
+kubectl apply -f ./API/api-deployment.yml
+
+# Frontend
+kubectl apply -f ./Frontend/frontend-service.yml
+kubectl apply -f ./Frontend/frontend-deploy.yml
