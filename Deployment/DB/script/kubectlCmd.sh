e25a306157eb2deabd879c1dd4c9c2734360187c
James <james.smith@heig-vd.ch>
2019-01-11 16:01:01 +0100

    ci and script db patch

--- Deployment/DB/script/kubectlCmd.sh
+++ Deployment/DB/script/kubectlCmd.sh
@@ -8,9 +8,9 @@ gcloud auth activate-service-account --key-file=./gc-key.json # put your gc-key 
 gcloud container clusters get-credentials $GC_CLUSTER_ID --zone $GC_ZONE --project $GC_PROJECT_ID # will take the information from the .env in the same directory
 
 # Run the kubectl cmd to change the db with the db into the folder dataBase
-kubectl exec foodlocal-db-mongodb-replicaset-0 -- rm -r dumpDBProd.agz
+kubectl exec foodlocal-db-mongodb-replicaset-0 -- rm -r /tmp/dumpDBProd.agz
 kubectl exec foodlocal-db-mongodb-replicaset-0 -- mongo api_foodlocal_production --eval "db.dropDatabase()"
 kubectl exec foodlocal-db-mongodb-replicaset-0 -- mongo api_foodlocal_dev --eval "db.dropDatabase()"
 kubectl exec foodlocal-db-mongodb-replicaset-0 -- mongo api_foodlocal_test --eval "db.dropDatabase()"
-kubectl cp dumpDBProd.agz foodlocal-db-mongodb-replicaset-0:/dumpDBProd.agz
-kubectl exec foodlocal-db-mongodb-replicaset-0 -- mongorestore --verbose --gzip --archive=/dumpDBProd.agz
+kubectl cp dumpDBProd.agz foodlocal-db-mongodb-replicaset-0:/tmp/dumpDBProd.agz
+kubectl exec foodlocal-db-mongodb-replicaset-0 -- mongorestore --verbose --gzip --archive=/tmp/dumpDBProd.agz
