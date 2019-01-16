9451ad0311e12d4ec7ae10233a08e1d651c114a2
James <james.smith@heig-vd.ch>
2019-01-16 14:58:59 +0100

    remake of deployment section/scripts and doc

--- Deployment/deployFrontAndBack.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-kubectl apply -f ./Ingress/ingress.yml
-kubectl apply -f ./API/api-service.yml
-kubectl apply -f ./API/api-deployment.yml
-kubectl apply -f ./Frontend/frontend-service.yml
-kubectl apply -f ./Frontend/frontend-deploy.yml
